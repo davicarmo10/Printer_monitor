20c8024c1c8626956469f18bdad4651dc6df95f6
davicarmo10 <54999246+davicarmo10@users.noreply.github.com>
2025-01-10 17:32:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,82 +3,79 @@ import time
 from openpyxl import Workbook, load_workbook
 from datetime import datetime
 
-register_path = r"\\192.168.2.8\isos\Relatorios\arquivo_com_data_e_hora.xlsx"
-printer_name = "4BARCODE 4B-2082A (Copiar 4)"
+register_path = r"\\Path\to\save\logs.xslx"
+printer_name = "Printer Name"
 rather_document = "Fast Report Document"
 
-# Função para inicializar ou carregar o arquivo Excel
+# Function to initialize the Excel file
 def init_excel():
     try:
-        # Tentar carregar o arquivo existente
+        # try to load the file
         workbook = load_workbook(register_path)
         sheet = workbook.active
     except FileNotFoundError:
-        # Se o arquivo não existe, criar um novo
+        # if doesnt exist, create a new file
         workbook = Workbook()
         sheet = workbook.active
-        sheet.title = "Registros de Impressão"
-        sheet.append(["Data/Hora", "ID do Trabalho", "Documento", "Usuário", "Status"])
+        sheet.title = "Logs of Print"
+        sheet.append(["Date/time", "ID", "Document", "User", "Status"])
         workbook.save(register_path)
     return workbook, sheet
 
-# Função para carregar os IDs já registrados
+# Function to read the IDs of the jobs already saved in the Excel file
 def load_id(sheet):
-    ids_registrados = set()
+    add_ids = set()
     for row in sheet.iter_rows(min_row=2, max_col=2, values_only=True):
-        ids_registrados.add(row[0])  # Adicionar o ID do trabalho na lista
-    return ids_registrados
+        add_ids.add(row[0])  # Adicionar o ID do trabalho na lista
+    return add_ids
 
-# Abrir conexão com a impressora
+# Open conection with printer and monitor
 
 def printer_monitor (printer_name_in):
     
     try:
         nPrinter = win32print.OpenPrinter(printer_name_in)
         workbook, sheet = init_excel()
-        ids_registrados = load_id(sheet)
+        ids = load_id(sheet)
 
-        # Obter informações sobre a impressora
-        #printer_info = win32print.GetPrinter(hPrinter, 2)
+        # Get info about the printer
 
         while True:
-            print("-----------------------Aguardando trabalho de impressão--------------------------")
+            print("-----------------------Waiting job--------------------------")
             jobs = win32print.EnumJobs(nPrinter, 0, -1, 2)  
             if jobs:
                 for job in jobs: 
                     job_id = job['JobId']
-                    documento = job['pDocument']
+                    document = job['pDocument']
                     
-                    if documento == rather_document:
-                        if job_id not in ids_registrados: #verifica ids
-                            #Salva no documento
-                            data_hora = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                            documento = job['pDocument']
-                            usuario = job['pUserName']
-                            status = job.get('Status', 'Desconhecido')
+                    if document == rather_document:
+                        if job_id not in ids: #verifica ids
+                            # Get infos of the print
+                            date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                            document = job['pDocument']
+                            user = job['pUserName']
+                            status = job.get('Status', 'Unknown')
 
-                            #Exibe na tela
-                            print(f"\nPáginas Impressas: {job['PagesPrinted']}")
-                            print(f"ID do Trabalho: {job['JobId']}")
+                            #Show in console
+                            print(f"\nPrinted pages: {job['PagesPrinted']}")
+                            print(f"Job ID: {job['JobId']}")
                             print(f"Status: {job['Status']}")
-                            print(f"Documento: {job['pDocument']}\n")
+                            print(f"Document: {job['pDocument']}\n")
 
-                            # Salvar no Excel
-                            sheet.append([data_hora, job_id, documento, usuario, status])
-                            ids_registrados.add(job_id)
+                            # Save document in Excel file
+                            sheet.append([date_time, job_id, document, user, status])
+                            ids.add(job_id)
                             workbook.save(register_path)    
             else:
-                print(f"Sem trabalhos na fila para a impressora '{printer_name_in}'.")
+                print(f"No jobs in a row '{printer_name_in}'.")
             time.sleep(5)
            
-        
     except Exception as e:
-        print(f"erro")
+        print(f"error")
 
-# Fechar conexão
+# close conection with printer
     finally:
         win32print.ClosePrinter(nPrinter)
-        #workbook.save(register_path)
 
 if __name__ == '__main__':
     printer_monitor(printer_name)
